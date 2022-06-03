96da995bcf27c2f96f3821e2bc219e8fe9df911b
michallangier <m.langier@wp.pl>
2022-06-03 02:49:00 +0200

    timer with hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [seconds, setSeconds] = useState(0)
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setSeconds(seconds + 1)
+    }, 1000)
+    return () => {
+      clearInterval(interval);
+    }
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Application works {seconds} [s].
+    </>
   );
 }
 
